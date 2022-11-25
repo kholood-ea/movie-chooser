c0f6697a497475f4588a4a0f68cbfb98b586c1a5
kholood <kholood.eassa@leannode.com>
2022-11-25 12:26:25 +0200

    initiate app header

---
+++ components/Header/Header.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+import { Text, SafeAreaView, View, StyleSheet } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+
+export default ({ children, navigation }) => {
+  return (
+    <SafeAreaView>
+      <View style={style.header}>
+        <View style={style.iconView}>
+          <Ionicons
+            name="chevron-back-sharp"
+            size={30}
+            color="white"
+            onPress={() => console.log("pressed")}
+          />
+        </View>
+      </View>
+      {children}
+    </SafeAreaView>
+  );
+};
+
+const style = StyleSheet.create({
+  header: {
+    backgroundColor: "black",
+    width: "100%",
+    height: 60,
+    justifyContent: "space-between",
+  },
+  iconView: {
+    padding: 10,
+  },
+});
--- screens/Heros/Heros.js
+++ screens/Heros/Heros.js
@@ -1,11 +1,10 @@
 import React, { Component } from "react";
 import { View, Text } from "react-native";
-import * as data from "./herosPlaceHolder.json";
-
+import { Header } from "../../components/Header";
 export default function Heros({ navigation }) {
   return (
     <>
-      <Text>Heros</Text>
+      <Header></Header>
     </>
   );
 }
