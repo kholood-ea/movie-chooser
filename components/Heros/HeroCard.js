01994bdac6f1f8d999677a84e3c3df60f34baf4f
kholood <kholood.eassa@leannode.com>
2022-11-26 12:17:52 +0200

    feature: navigate user to movie on hero select

--- components/Heros/HeroCard.js
+++ components/Heros/HeroCard.js
@@ -1,20 +1,18 @@
-import React, { Component, useState } from "react";
-import {
-  Text,
-  Image,
-  StyleSheet,
-  TouchableOpacity,
-  Pressable,
-} from "react-native";
+import React, { useState } from "react";
+import { Text, Image, StyleSheet, Pressable } from "react-native";
 import * as Animatable from "react-native-animatable";
 
 export default (props) => {
-  const { hero } = props;
+  const { hero, onHeroPress } = props;
   const [animation, setAnimation] = useState("bounceIn");
 
   const handleAnimation = () => {
     setAnimation("jello");
+    setTimeout(() => {
+      onHeroPress();
+    }, 500);
   };
+
   return (
     <Pressable style={style.card} onPress={() => handleAnimation()}>
       <Animatable.View animation={animation} style={style.container}>
@@ -27,15 +25,11 @@ export default (props) => {
 const style = StyleSheet.create({
   card: {
     flex: 1,
-    // width: "50%",
-    // height: 150,
+
     alignItems: "center",
     marginVertical: 3,
   },
   container: {
-    // width: "100%",
-    // height: "95%",
-    // justifyContent: "space-around",
     alignItems: "center",
   },
   heroImage: {
