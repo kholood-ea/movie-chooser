734a21deb40cb394713319f88578fdc0e83957b7
kholood <kholood.eassa@leannode.com>
2022-11-26 17:12:12 +0200

    implement selected movies screen

--- screens/SelectedMovies/Handler.js
+++ screens/SelectedMovies/Handler.js
@@ -1,3 +1,31 @@
-export default () => {
-  return {};
+import { useQuery } from "react-query";
+import { useState, useEffect } from "react";
+export default ({ route }) => {
+  const [shuffeling, setShuffeling] = useState(false);
+  const [movies, setMovies] = useState([]);
+  const { hero } = route.params;
+  const { isLoading, error, data } = useQuery("repoData", () =>
+    fetch(`https://www.omdbapi.com/?s=${hero.name}&apikey=737e2b80`).then(
+      (res) => res.json()
+    )
+  );
+  const shuffle = (movies) => {
+    setShuffeling(true);
+    let shuffeledMovies = movies;
+    for (var i = shuffeledMovies?.length - 1; i > 0; i--) {
+      var j = Math.floor(Math.random() * (i + 1));
+      var temp = shuffeledMovies[i];
+      shuffeledMovies[i] = shuffeledMovies[j];
+      shuffeledMovies[j] = temp;
+    }
+    setMovies(shuffeledMovies);
+    setTimeout(() => {
+      setShuffeling(false);
+    }, 1000);
+  };
+  useEffect(() => {
+    shuffle(data?.Search);
+  }, [data]);
+
+  return { shuffeling, movies, shuffle };
 };
--- screens/SelectedMovies/SelectedMovies.js
+++ screens/SelectedMovies/SelectedMovies.js
@@ -1,10 +1,42 @@
-import React, { Component } from "react";
+import React, { Component, useRef } from "react";
 import { View, Text } from "react-native";
+import { Header } from "../../components/Header";
+import { SelectedMovieList, ShuffleButton } from "./components";
+import LottieView from "lottie-react-native";
 
-export default function SelectedMovies({ navigation }) {
+import Handler from "./Handler";
+import * as data from "./moviesPlaceHolder.json";
+export default function SelectedMovies({ navigation, route }) {
+  const { shuffeling, movies, shuffle } = Handler({ route });
+  const animation = useRef(null);
+
   return (
     <>
-      <Text>SelectedMovies</Text>
+      <Header
+        navigation={navigation}
+        Back
+        title={"Movie For You"}
+        onBackPress={() => {
+          navigation.pop();
+        }}
+      >
+        {shuffeling && (
+          <LottieView
+            autoPlay
+            ref={animation}
+            style={{
+              marginTop: 40,
+              width: 225,
+              height: 225,
+            }}
+            source={
+              require("../../assets/animations/moviesWheel.json") //lottiefiles.com/featured
+            }
+          />
+        )}
+        {!shuffeling && <SelectedMovieList movies={movies} />}
+      </Header>
+      <ShuffleButton onShuffle={() => shuffle(movies)} />
     </>
   );
 }
